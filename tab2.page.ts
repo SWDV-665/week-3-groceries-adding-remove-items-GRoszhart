d734c974e9210cd74f71bb832a5d392798ae9527
GRoszhart <136271882+GRoszhart@users.noreply.github.com>
2023-07-20 11:38:41 -0500

    Add files via upload

---
+++ tab2.page.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  title = "Groceries";
+
+  items = [
+    {
+      name: "Cheese",
+      quantity: 3
+    },
+    {
+      name: "Milk",
+      quantity: 2
+    },
+    {
+      name: "Waffles",
+      quantity: 5
+    },
+    {
+      name: "Bacon",
+      quantity: 1
+    },
+  ];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
+
+  }
+
+  async removeItem(item: any, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = await this.toastCtrl.create({
+      message: 'Removing Item - ' + item.name,
+      duration: 1500
+    });
+
+    await toast.present();
+
+    this.items.splice(index, 1);
+  }
+
+  addItem() {
+    console.log("Adding Item");
+    this.showAddItemPrompt();
+  }
+
+  async showAddItemPrompt() {
+    const prompt = await this.alertCtrl.create({
+      header: 'Add Item',
+      message: 'Please enter item below',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    await prompt.present();
+  }
+
+}
